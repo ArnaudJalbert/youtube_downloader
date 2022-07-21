879d03e2404447608b5221da88738b1fb39a6c1a
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2022-07-21 11:28:06 -0400

    Upgraded the constructor.
    Initializing all parameters with functions instead of directly in the constructor.
    
    Method to downlaod the video according to parameters.

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from yt_downl import yt_downl
 
 url = 'https://www.youtube.com/watch?v=g9xvkAnfh0w&ab_channel=iJustine'
 export_path = '/Users/arnaudjalbert/Projects/youtube_downloader/dump'
-name = 'test'
+file_name = "Arno"
 file_type = 'video'
 
-test = yt_downl(url, export_path, name, file_type)
+test = yt_downl(url, export_path=export_path,
+                file_name=file_name, file_type=file_type)
 
-print(test.get_author())
+print(test.to_string())
+
+test.downl_video()
--- yt_downl.py
+++ yt_downl.py
@@ -1,13 +1,48 @@
 from pytube import YouTube
+from os import path
+
+deft_export_path = path.normpath(path.expanduser("~/Desktop"))
+
+deft_file_type = 'video'
 
 
 class yt_downl(YouTube):
 
-    def __init__(self, url, export_path, name, file_type):
+    def __init__(self, url, export_path=deft_export_path, file_name=None, file_type=deft_file_type):
+
         super().__init__(url)
 
-        self.export_path = export_path
+        self.set_export_path(export_path)
 
-        self.name = name
+        self.set_name(file_name)
 
-        self.file_type = file_type
+        self.set_file_type(file_type)
+
+    def set_export_path(self, export_path):
+        if path.isdir(export_path):
+            self.export_path = export_path
+        else:
+            print("Not a valid path! Will set the desktop as the export path.")
+            self.export_path = path.normpath(path.expanduser("~/Desktop"))
+
+    def set_name(self, file_name):
+        if isinstance(file_name, str):
+            self.file_name = file_name+".mp4"
+        else:
+            print("Not a string! Default will be applied.")
+            self.file_name = self.title
+
+    def set_file_type(self, file_type):
+        if file_type == "audio" or file_type == "video":
+            self.file_type = file_type
+        else:
+            print("Not a string! Default will be applied.")
+            self.file_type = 'video'
+
+    def to_string(self):
+        return "Name : " + self.file_name + ", file type : " + self.file_type + " and export path : " + self.export_path + "."
+
+    def downl_video(self):
+        to_downl = self.streams.get_highest_resolution()
+        to_downl.download(output_path=self.export_path,
+                          filename=self.file_name)
